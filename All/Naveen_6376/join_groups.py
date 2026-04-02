58fcc5d82e4323b481f3c53f7737afc484978116
akashk1947 <theakash1947@gmail.com>
2026-04-02 21:38:47 +0530

    2nd-april

--- All/Naveen_6376/join_groups.py
+++ All/Naveen_6376/join_groups.py
@@ -10,11 +10,10 @@ api_id = 38713917
 api_hash = "66b6d1829c3cf7d38637005a6acbf0c4"
 
 session_name = "session"
-
 # ========= TIMING =========
 BATCH_SIZE = 5
-SHORT_WAIT = (300, 600)   # 5 between joins
-LONG_WAIT = 500           # 5 minutes rest after batch
+SHORT_WAIT = (300, 600)   # 5-10 minutes between joins
+LONG_WAIT = 700           # 11 minutes 40 seconds rest after batch
 
 # ========= LOAD GROUPS =========
 with open("groups.txt", "r", encoding="utf-8") as f:
@@ -23,6 +22,8 @@ with open("groups.txt", "r", encoding="utf-8") as f:
 async def main():
     client = TelegramClient(session_name, api_id, api_hash)
     await client.start()
+    me = await client.get_me()
+    account_label = me.username or me.first_name or Mobile
 
     joined = set()
     while True:
@@ -34,20 +35,20 @@ async def main():
             if group in joined:
                 continue
             try:
-                print(f"➡️ RajRajak_[{idx}/{len(groups)}] Joining: {group}")
+                print(f"➡️ {account_label}_[{idx}/{len(groups)}] Joining: {group}")
                 await client(JoinChannelRequest(group))
 
                 join_count += 1
                 batch_count += 1
                 joined.add(group)
-                print(f"✅ Joined total: {join_count}")
+                print(f"✅ {account_label}_Joined total: {join_count}")
 
                 # Random 5–10 minute wait
                 await asyncio.sleep(random.randint(*SHORT_WAIT))
 
-                # Batch rest: fixed 10 minutes
+                # Batch rest: fixed 11 minutes 40 seconds
                 if batch_count >= BATCH_SIZE:
-                    print("😴 Batch complete. Resting 5 minutes")
+                    print("😴 Batch complete. Resting 11 minutes 40 seconds")
                     await asyncio.sleep(LONG_WAIT)
                     batch_count = 0
 
@@ -57,7 +58,6 @@ async def main():
                 wait_hours = wait_minutes // 60
                 wait_minutes = wait_minutes % 60
                 print(f"⛔ JOIN FLOODWAIT: {wait_hours} : {wait_minutes} : {wait_seconds}")
-                # print(f"🛑 Flood wait detected. Sleeping for {e.seconds} seconds...")
                 await asyncio.sleep(e.seconds + 5)  # add a buffer
                 print("🔄 Resuming join process after flood wait.")
                 break  # break inner for-loop, restart outer while-loop
